f93281c22f1218738442101047ed3318be050c8b
Atila Guilherme <atilacarrao@hotmail.com>
2022-07-05 09:15:28 -0300

    initial_folders

---
+++ src/components/layout/Container.js
@@ -0,0 +1,11 @@
+import styles from './Container.module.css'
+
+const Container = () => {
+  return (
+    <div className={styles.container}>
+      pp
+    </div>
+  )
+}
+
+export default Container
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,15 @@
+import Container from "./Container";
+import styles from "./Navbar.module.css";
+// rafce
+
+const Navbar = () => {
+  return (
+    <nav>
+      <Container>
+      
+      </Container>
+    </nav>
+  )
+}
+
+export default Navbar
